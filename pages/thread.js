e90757ee4b0fcd47117b6761d88163748afad660
OhNotreDame <jbonnard@outlook.com>
2017-05-01 22:12:20 +0200

    Improving thread messages display

--- pages/thread.js
+++ pages/thread.js
@@ -26,7 +26,7 @@ function renderThreadContent() {
 
 function addMessageToThreadDisplay(thread) {
 
-	var firstMsgOfThread = getLastMessageOfThread(thread);
+	var firstMsgOfThread = getFirstMessageOfThread(thread);
 	var threadHeaders = firstMsgOfThread.payload.headers;
 	var threadLabels = firstMsgOfThread.labelIds;
 	
@@ -80,95 +80,95 @@ function addMessageToThreadDisplay(thread) {
 	
 	var messageCount = thread.messages.length;
 	console.log(messageCount);
-	for (var i = 0; i < messageCount; i++) {
+	for (var i = 0; i < messageCount; i++) 
+	{
 
 		var message = thread.messages[i];
 		var messageHeaders = message.payload.headers;
 		var messageLabelIds = message.labelIds;
 
-		
-		 // Extract From value, and parse it if necessary
-     var fromMail = getHeader(messageHeaders, 'From');
-     var tmp_from = fromMail.match("<(.*)>");
-     if (tmp_from) {
-          fromMail = tmp_from[1];
-     }
+
+		// Extract From value, and parse it if necessary
+		var fromMail = getHeader(messageHeaders, 'From');
+		var tmp_from = fromMail.match("<(.*)>");
+		if (tmp_from) {
+			fromMail = tmp_from[1];
+		}
 		
 
-	var fromContact = createContact(getHeader(messageHeaders, 'From')) ;
-	var toContact = createContact(getHeader(messageHeaders, 'To')) ;
-	var ccContact = createContact(getHeader(messageHeaders, 'CC')) ;
+		var fromContact = createContact(getHeader(messageHeaders, 'From')) ;
+		var toContact = createContact(getHeader(messageHeaders, 'To')) ;
+		var ccContact = createContact(getHeader(messageHeaders, 'CC')) ;
 	
 
-	var globalToMailTxt = toContact.fullName;
-	if (ccContact.fullName)
-	{
-		globalToMailTxt = toContact.fullName + ", " + ccContact.fullName;
-	}
-
+		var globalToMailTxt = toContact.fullName;
+		if (ccContact.fullName)
+		{
+			globalToMailTxt = toContact.fullName + ", " + ccContact.fullName;
+		}
 	
-	var mailDateString = getHeader(messageHeaders, 'Date');
-	var mailDate = new Date(mailDateString);
-	var finalMailDate = mailDate.toLocaleString("en-GB");
+		var mailDateString = getHeader(messageHeaders, 'Date');
+		var mailDate = new Date(mailDateString);
+		var finalMailDate = mailDate.toLocaleString("en-GB");
 
-	// Add message to thread
-	$('#msg-accordion').append(
-		'<div class="msg-row panel panel-default" id="msg-row-' + i + '">\
-			<div class="msg-header panel-heading" role="tab" id="msg-header-'+ i + '" >\
-				<h4 class="panel-title">\
-					<div id="msg-reply-' + i + '" class="pull-right">\
-							&nbsp; <button type="button" class="reply-button" id="reply-button-' + thread.id + '">\
-								<img id="reply-icon-' + thread.id + '" src="../img/reply.png" title="Reply"/>\
-							</button>\
-							&nbsp; <button type="button" class="reply-button" id="replyall-button-' +thread.id + '">\
-								<img id="replyall-icon-' + thread.id + '" src="../img/replyall.png" title="Reply"/>\
-							</button>&nbsp; \
-					</div>\
-					<a data-toggle="collapse" data-parent="#msg-accordion" href="#msg-body-'+ i +'" aria-expanded="true" aria-controls="#msg-body-'+ i +'" >\
-						<div id="msg-from-' + i + '" class="msg-from pull-left">  '+ fromContact.fullName + ' </div>\
-						<div id="msg-date-' + i + '" class="msg-date pull-right"> ' + finalMailDate + ' </div>\
-						<br/><div id="msg-to-' + i + '" class="msg-from-small pull-left"> ' + globalToMailTxt + ' </div>\
-					</a>\
-				<\h4>\
-			</div>\
-			<div id="msg-hidden-info-'+ i +' " class="hidden">\
-				<div id="msg-hidden-fromMail-'+ i +' " class="hidden"> ' + fromContact.emailAddress  + ' </div>\
-				<div id="msg-hidden-toMail-'+ i +' " class="hidden"> '+ toContact.emailAddress  + ' </div>\
-				<div id="msg-hidden-ccMail-'+ i +' " class="hidden"> '+ ccContact.emailAddress  + ' </div>\
-			</div>\
-		<div id="msg-body-' + i + '" class="collapse msg-body" role="tabpanel" aria-labelledby="#msg-body-'+ i + '">'+ getBody(message.payload) + '</div>\
-		</div>');
+		// Add message to thread
+		$('#msg-accordion').append(
+			'<div class="msg-row panel panel-default" id="msg-row-' + i + '">\
+				<div class="msg-header panel-heading" role="tab" id="msg-header-'+ i + '" >\
+					<h4 class="panel-title">\
+						<div id="msg-reply-' + i + '" class="pull-right">\
+								&nbsp; <button type="button" class="reply-button" id="reply-button-' + thread.id + '">\
+									<img id="reply-icon-' + thread.id + '" src="../img/reply.png" title="Reply"/>\
+								</button>\
+								<button type="button" class="reply-button" id="replyall-button-' +thread.id + '">\
+									<img id="replyall-icon-' + thread.id + '" src="../img/replyall.png" title="Reply"/>\
+								</button>\
+						</div>\
+						<a data-toggle="collapse" data-parent="#msg-accordion" href="#msg-body-'+ i +'" aria-expanded="true" aria-controls="#msg-body-'+ i +'" >\
+							<div id="msg-from-' + i + '" class="msg-from pull-left">  '+ fromContact.fullName + ' </div>\
+							<div id="msg-date-' + i + '" class="msg-date pull-right"> ' + finalMailDate + ' </div>\
+							<br/><div id="msg-to-' + i + '" class="msg-from-small pull-left"> ' + globalToMailTxt + ' </div>\
+						</a>\
+					<\h4>\
+				</div>\
+				<div id="msg-hidden-info-'+ i +' " class="hidden">\
+					<div id="msg-hidden-fromMail-'+ i +' " class="hidden"> ' + fromContact.emailAddress  + ' </div>\
+					<div id="msg-hidden-toMail-'+ i +' " class="hidden"> '+ toContact.emailAddress  + ' </div>\
+					<div id="msg-hidden-ccMail-'+ i +' " class="hidden"> '+ ccContact.emailAddress  + ' </div>\
+				</div>\
+			<div id="msg-body-' + i + '" class="collapse msg-body" role="tabpanel" aria-labelledby="#msg-body-'+ i + '">'+ getBody(message.payload) + '</div>\
+			</div>');
 	}
 	
 	// Expand last thread message
 	var lastMsgToShow = "#msg-body-"+(messageCount-1);
 	$(lastMsgToShow).addClass("collapse in");
-	
-	
+
+
 	/*  Add js event handler on Delete Main Button */
-     $('#asread-button-' + thread.id).on('click', function () {
+	$('#asread-button-' + thread.id).on('click', function () {
 		markThreadAsRead(thread.id, null);
 		$('#asread-button-' + thread.id).hide();
 		$('#threadUnread').addClass('hidden');
 		$('#threadUnread').hide();
-     });
-	 
-	    /* Add js event handler on Delete Main Button */
-     $('#delete-button-' + thread.id).on('click', function () {
-         sendThreadToTrash(thread.id, null);
-		 document.location.href="inbox.html";
-     });
-	 
-	 /* Add js event handler on Reply Main Button */
-     $('#reply-button-' + thread.id).on('click', function () {
-          console.log("reply to this thread");
-     });
-	 
-	 	 
-	 /* Add js event handler on Reply Main Button */
-     $('#replyall-button-' + thread.id).on('click', function () {
-          console.log("reply all to this thread");
-     });
+	});
+
+	/* Add js event handler on Delete Main Button */
+	$('#delete-button-' + thread.id).on('click', function () {
+		sendThreadToTrash(thread.id, null);
+		document.location.href="inbox.html";
+	});
+
+	/* Add js event handler on Reply Main Button */
+	$('#reply-button-' + thread.id).on('click', function () {
+		console.log("reply to this thread");
+	});
+
+
+	/* Add js event handler on Reply Main Button */
+	$('#replyall-button-' + thread.id).on('click', function () {
+		console.log("reply all to this thread");
+	});
 	 
 }
 
