47fe70d69aac615ed3ea9d199bfdb0018c7a2a9a
OhNotreDame <jbonnard@outlook.com>
2017-05-06 16:20:52 +0200

    Options

--- pages/drive.js
+++ pages/drive.js
@@ -5,64 +5,36 @@ window.onload = function () {
 
 /* Load Gmail API, and when it's done, call displayInbox */
 function loadDriveAPI() {
-	gapi.client.load('drive', 'v2', renderFiles);
+	gapi.client.load('drive', 'v3', renderFiles);
 }
 
 /* renderFiles() */
 function renderFiles() {
-   	prepareToolbar();
+   	console.log("drive/renderFiles");
+	prepareToolbar();
 	$('#table-files > tbody').empty();
 	//retrieveAllFiles("mimeType = 'application/pdf'", null, 30);
-	retrieveAllFilesInFolder('root', '', null, 5);
-}
-
-
-
-
-function retrieveAllFilesInFolder(folderId, q, orderBy, maxResults) {
-  var retrievePageOfChildren = function(request, result) {
-    request.execute(function(resp) {
-      result = result.concat(resp.items);
-      var nextPageToken = resp.nextPageToken;
-      if (nextPageToken) {
-        request = gapi.client.drive.children.list({
-			'folderId' : folderId,
-			'key': apiKEY, 
-			'q': q,
-			'orderBy' : orderBy, 
-			'maxResults': maxResults,			
-			'pageToken': nextPageToken
-        });
-        retrievePageOfChildren(request, result);
-      } else {
-        parseFilesInFolder(folderId, result);
-      }
-    });
-  }
-  var initialRequest = gapi.client.drive.children.list({
-		'folderId' : folderId,
-		'key': apiKEY, 
-		'q': q,
-		'orderBy' : orderBy, 
-		'maxResults': maxResults,
-    });
-  retrievePageOfChildren(initialRequest, []);
+	//retrieveAllFiles('root', null, 5);
+	//retrieveAllFiles("mimeType = 'application/pdf'", 'user', 'drive', 5);
+	retrieveAllFiles(null, 'user', 'drive', null);
 }
 
 
 /* retrieveAllFiles() */
-function retrieveAllFiles(q, orderBy, maxResults) {
+function retrieveAllFiles(q, corpora, spaces, orderBy) {
+	console.log("drive/retrieveAllFiles");
 	var getPageOfFilesLists = function (request, result) {
           request.execute(function (resp) {
                result = result.concat(resp.items);
                var nextPageToken = resp.nextPageToken;
                if (nextPageToken) {
-                    request = gapi.client.drive.files.list({
-							'userId': USER,
+                    request = gapi.client.drive.files.list({			
+							'key': apiKEY,
 							'pageToken': nextPageToken,
 							'q': q,
-							'orderBy' : orderBy, 
-							'maxResults': maxResults
+							'corpora': corpora,
+							'spaces': spaces,
+							'orderBy' : orderBy
                          });
                     getPageOfFilesLists(request, result);
                } else {
@@ -72,66 +44,29 @@ function retrieveAllFiles(q, orderBy, maxResults) {
      };
 
 		var	request = gapi.client.drive.files.list({
-							'userId': USER,
+							'key': apiKEY,
 							'q': q,
-							'orderBy' : orderBy, 
-							'maxResults': maxResults
+							'corpora': corpora,
+							'spaces': spaces,
+							'orderBy' : orderBy
                          });
      getPageOfFilesLists(request, []);
 }
 
-/* parseFilesInFolder() */
-function parseFilesInFolder(folderId, files) {
-     $.each(files, function () {          
-		// addFileToTable(this);
-		  getFileInFolder(folderId, this.id);
-     });
-}
-
-/* isFileInFolder() */
-function isFileInFolder(folderId, fileId, callback) {
-  var request = gapi.client.drive.children.get({
-    'folderId': folderId,
-    'childId': fileId
-  });
-  request.execute(function(resp) {
-    if (resp.error) {
-      if (resp.error.code = 404) {
-        callback(false);
-      } else {
-        console.log('An error occurred: ' + resp.error.message);
-        callback(null);
-      }
-    } else {
-      callback(true);
-    }
-  });
-}
-
 /* parseFiles() */
 function parseFiles(files) {
-     $.each(files, function () {          
-		  getFile(this.id);
+    console.log("drive/parseFiles");
+	$.each(files, function () {      
+		console.log("drive/parseFiles("+ this.id+")");    
+		getFile(this.id);
      });
 }
 
 /* getTasks() */
 function getFile(fileId) {
-     var request = gapi.client.drive.files.get({
+    console.log("drive/getFile("+ fileId+")");
+	var request = gapi.client.drive.files.get({
                'fileId': fileId
-          });
-     request.execute(function (resp) {
-			addFileToTable(resp);          
-     });
-}
-
-
-/* getTasks() */
-function getFileInFolder(folderId, fileId) {
-     var request = gapi.client.drive.children.get({
-              'key': apiKEY, 
-			  'folderId': folderId,
-			'childId': fileId
           });
      request.execute(function (resp) {
 			addFileToTable(resp);          
@@ -140,44 +75,23 @@ function getFileInFolder(folderId, fileId) {
 
 /* addFileToTable() */
 function addFileToTable(file) {
-	
-	var objType = file.mimeType;
-	
-	if (objType.indexOf("folder") >= 0)
+
+	 console.log("drive/addFileToTable("+ file.title +")");
+	if (file.title)
 	{
-		if (file.title)
-		{
-			var lstModified = new Date(file.modifiedDate);
-			var lstModifiedFormatted = lstModified.toLocaleString("en-GB");		
-			$('#table-files > tbody').append(
-				  '<tr class="file_item" id="row-' + file.id + '">\
-				  <td><a href="' + file.webViewLink+'">'+ file.title + '</a></td>\
-				  <td> Folder </td>\
-				  <td>' + lstModifiedFormatted + '</td>\
-				  <td>' + file.lastModifyingUserName + '</td>\
-				  </tr >');
-				  // downloadFile(file, null)
-		}	
+		var lstModified = new Date(file.modifiedDate);
+		var lstModifiedFormatted = lstModified.toLocaleString("en-GB");		
+		$('#table-files > tbody').append(
+			  '<tr class="file_item" id="row-' + file.id + '">\
+			  <td><a href="' + file.webContentLink+'">'+ file.originalFilename + '</a></td>\
+			  <td>' + file.fileExtension + '</td>\
+			  <td>' + lstModifiedFormatted + '</td>\
+			  <td>' + file.lastModifyingUserName + '</td>\
+			  </tr >');
+			  // downloadFile(file, null)
+	}	
 		
-	}
-	else
-	{
-		if (file.title)
-		{
-			var lstModified = new Date(file.modifiedDate);
-			var lstModifiedFormatted = lstModified.toLocaleString("en-GB");		
-			$('#table-files > tbody').append(
-				  '<tr class="file_item" id="row-' + file.id + '">\
-				  <td><a href="' + file.webContentLink+'">'+ file.originalFilename + '</a></td>\
-				  <td>' + file.fileExtension + '</td>\
-				  <td>' + lstModifiedFormatted + '</td>\
-				  <td>' + file.lastModifyingUserName + '</td>\
-				  </tr >');
-				  // downloadFile(file, null)
-		}	
-		
-	}
-	  
+	
     
 	/* Reinforce sort */
 	$('#table-files > tbody').tablesorter({
--- pages/options.js
+++ pages/options.js
@@ -1,4 +1,3 @@
 window.onload = function () {
-	console.log("options");
- 	$('#myContainer').show();
-});
+	prepareToolbar();
+}
